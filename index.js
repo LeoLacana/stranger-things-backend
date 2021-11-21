02c0d9ebde31dce6512095d3984ed1904d6b047d
LeoLacana <wilson17414@gmail.com>
2021-11-21 06:48:32 -0300

    teste deploy

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config();
 
-const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
-const StrangerThingsRepository = require('./data/repository/StrangerThings');
-const StrangerThingsService = require('./services/StrangerThings');
+// const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+// const StrangerThingsRepository = require('./data/repository/StrangerThings');
+// const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
-);
-const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
-);
+// const strangerThingsRepository = new StrangerThingsRepository(
+//   strangerThingsDataset,
+// );
+// const strangerThingsService = new StrangerThingsService(
+//   strangerThingsRepository,
+// );
 
-app.use(cors());
+// app.use(cors());
 
-// const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE === 'true';
+// // const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE === 'true';
 
-const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE);
+// const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE);
 
-app.get('/', (req, res) => {
-  const characters = strangerThingsService.search(
-    req.query,
-    hereIsTheUpsideDown,
-  );
+// app.get('/', (req, res) => {
+//   const characters = strangerThingsService.search(
+//     req.query,
+//     hereIsTheUpsideDown,
+//   );
 
-  res.status(200).json(characters);
-});
+//   res.status(200).json(characters);
+// });
 
 // const PORT = process.env.PORT;
 
