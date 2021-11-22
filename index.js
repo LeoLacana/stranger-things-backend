66d3b2e5cfe0398bb7e0d5732731c7ddf3849b4f
LeoLacana <wilson17414@gmail.com>
2021-11-22 06:32:02 -0300

    não sei mais

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE);
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
